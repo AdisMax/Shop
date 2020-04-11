d1e0d7f7dc8acfd29ca3b69ae9e4f0317f419040
AdisMax <adisbravo@gmail.com>
2020-04-10 21:14:20 -0400

    Show prducts in Xamarin.Forms

--- Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
@@ -1,21 +1,29 @@
 ﻿namespace Shop.UIForms.ViewModels
 {
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
     using Shop.Common.Models;
     using Shop.Common.Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
-        private ApiService apiService;
+        private readonly ApiService apiService;
 
         private ObservableCollection<Product> products;
 
+        private bool isRefreshing;
+
         public ObservableCollection<Product> Products
         {
-            get { return this.products; }
-            set { this.SetValue(ref this.products, value); }
+            get => this.products;
+            set => this.SetValue(ref this.products, value);
+        }
+
+        public bool IsRefreshing
+        {
+            get => this.isRefreshing;
+            set => this.SetValue(ref this.isRefreshing, value);
         }
 
 
@@ -27,10 +35,13 @@
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
             var response = await this.apiService.GetListAsync<Product>(
                 "https://shopstarter.azurewebsites.net",
                 "/api",
                 "/Products");
+
+            this.IsRefreshing = false;
 
             if (!response.IsSuccess)
             {
